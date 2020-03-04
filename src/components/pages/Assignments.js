519fbd9d7733d9ff917bb40d1bb275a5f9088be6
tahreen zeba <tahreenzeba1@gmail.com>
2020-03-03 22:44:17 -0500

    Add component for Assingment link

---
+++ src/components/pages/Assignments.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Assignments = () => {
+    return (
+        <div>
+           <div className="paralsec3 ">
+                    <div className="sec3">
+                        <p>I'm a full stack developer, specializing in both front-end and back-end web application.
+                            Check out more about me and my most recent work below and feel free to get in touch.
+                        </p>
+            
+                    </div>
+                </div> 
+        </div>
+    )
+}
+
+export default Assignments;
